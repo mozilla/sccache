b916bfff1afe32b0710b5005479a6b8efb11f41d
Ted Mielczarek <ted@mielczarek.org>
2016-08-25 15:56:03 -0400

    use GNU parallel in upload-tooltool.sh if available

--- scripts/upload-tooltool.sh
+++ scripts/upload-tooltool.sh
@@ -6,12 +6,21 @@ if ! test -d "$1"; then
     exit 1
 fi
 
-tooltool=$(realpath "$(dirname $0)/tooltool.py")
-for d in "$1"/*; do
-    pushd $d
+export tooltool=$(realpath "$(dirname $0)/tooltool.py")
+upload_one() {
+    cd $1
     chmod -x sccache2.tar.*
     rm -f releng.manifest
     python "$tooltool" add -v --visibility=public --unpack -m releng.manifest sccache2.tar.*
     python "$tooltool" upload -v -m releng.manifest  --message "Build of sccache2 from revision $(cat REV)" --authentication-file ~/tooltool-upload-token
-    popd
-done
+}
+export -f upload_one
+
+dirs="$1"/*
+if which parallel >/dev/null; then
+    parallel --linebuffer upload_one ::: ${dirs}
+else
+  for d in ${dirs}; do
+      upload_one $d
+  done
+fi
