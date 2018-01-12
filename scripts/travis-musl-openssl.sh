e5ca772717e7b0cea24f489c00133dc5830fc48a
Ted Mielczarek <ted@mielczarek.org>
2018-01-12 10:29:26 -0500

    try fixing i686-musl one more time...

--- scripts/travis-musl-openssl.sh
+++ scripts/travis-musl-openssl.sh
@@ -3,17 +3,16 @@ set -ex
 
 case "$TARGET" in
     x86_64-*)
-        OPENSSL_TARGET=linux-x86_64
+        OPTIONS=(linux-x86_64)
         ;;
     i686-*)
-        OPENSSL_TARGET=linux-generic32
-        EXTRA=-m32
+        OPTIONS=(linux-generic32 -m32 -Wl,-melf_i386)
         ;;
 esac
 
 rustup target add "$TARGET"
 curl https://www.openssl.org/source/openssl-1.0.2l.tar.gz | tar xzf -
 cd openssl-1.0.2l
-CC=musl-gcc ./Configure --prefix="$OPENSSL_DIR" no-dso no-ssl2 no-ssl3 "$EXTRA" "$OPENSSL_TARGET" -fPIC
+CC=musl-gcc ./Configure --prefix="$OPENSSL_DIR" no-dso no-ssl2 no-ssl3 "${OPTIONS[@]}" -fPIC
 make -j"$(nproc)"
 make install
