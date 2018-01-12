e559fb724a892e7641228134011149fec2286b07
Ted Mielczarek <ted@mielczarek.org>
2018-01-12 09:21:40 -0500

    build travis releases with --features=all, add an i686 linux release build

---
+++ scripts/travis-musl-openssl.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -vex
+
+case "$TARGET" in
+    x86_64-*)
+        OPENSSL_TARGET=linux-x86_64
+        ;;
+    i686-*)
+        OPENSSL_TARGET=linux-generic32
+        EXTRA=-m32
+        ;;
+esac
+
+rustup target add "$TARGET"
+curl https://www.openssl.org/source/openssl-1.0.2l.tar.gz | tar xzf -
+cd openssl-1.0.2l
+CC=musl-gcc ./Configure --prefix="$OPENSSL_DIR" no-dso no-ssl2 no-ssl3 "$EXTRA" "$OPENSSL_TARGET" -fPIC
+make -j"$(nproc)"
+make install
