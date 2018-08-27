7d142ce658bb745ddd551d32aacebca9787fdb78
Markus Westerlind <markus.westerlind@distilnetworks.com>
2018-08-27 13:54:09 +0200

    Use Reqwest to access s3 to follow redirects
    Fixes #265

--- src/simples3/s3.rs
+++ src/simples3/s3.rs
@@ -11,10 +11,10 @@ use crypto::hmac::Hmac;
 use crypto::mac::Mac;
 use crypto::sha1::Sha1;
 use futures::{Future, Stream};
-use hyper::{self, header};
+use hyper::header;
 use hyper::Method;
-use hyper::client::{Client, Request, HttpConnector};
-use hyper_tls::HttpsConnector;
+use reqwest;
+use reqwest::unstable::async::{Client, Request};
 use simples3::credential::*;
 use time;
 use tokio_core::reactor::Handle;
@@ -32,12 +32,14 @@ pub enum Ssl {
 }
 
 fn base_url(endpoint: &str, ssl: Ssl) -> String {
-    format!("{}://{}/",
-            match ssl {
-                Ssl::Yes => "https",
-                Ssl::No => "http",
-            },
-            endpoint)
+    format!(
+        "{}://{}/",
+        match ssl {
+            Ssl::Yes => "https",
+            Ssl::No => "http",
+        },
+        endpoint
+    )
 }
 
 fn hmac<D: Digest>(d: D, key: &[u8], data: &[u8]) -> Vec<u8> {
@@ -47,7 +49,11 @@ fn hmac<D: Digest>(d: D, key: &[u8], data: &[u8]) -> Vec<u8> {
 }
 
 fn signature(string_to_sign: &str, signing_key: &str) -> String {
-    let s = hmac(Sha1::new(), signing_key.as_bytes(), string_to_sign.as_bytes());
+    let s = hmac(
+        Sha1::new(),
+        signing_key.as_bytes(),
+        string_to_sign.as_bytes(),
+    );
     base64::encode_config::<Vec<u8>>(&s, base64::STANDARD)
 }
 
@@ -55,7 +61,7 @@ fn signature(string_to_sign: &str, signing_key: &str) -> String {
 pub struct Bucket {
     name: String,
     base_url: String,
-    client: Client<HttpsConnector<HttpConnector>>,
+    client: Client,
 }
 
 impl fmt::Display for Bucket {
@@ -65,16 +71,12 @@ impl fmt::Display for Bucket {
 }
 
 impl Bucket {
-    pub fn new(name: &str, endpoint: &str, ssl: Ssl, handle: &Handle)
-        -> Result<Bucket>
-    {
+    pub fn new(name: &str, endpoint: &str, ssl: Ssl, handle: &Handle) -> Result<Bucket> {
         let base_url = base_url(&endpoint, ssl);
         Ok(Bucket {
             name: name.to_owned(),
             base_url: base_url,
-            client: Client::configure()
-                        .connector(HttpsConnector::new(1, handle)?)
-                        .build(handle),
+            client: Client::new(handle),
         })
     }
 
@@ -82,37 +84,46 @@ impl Bucket {
         let url = format!("{}{}", self.base_url, key);
         debug!("GET {}", url);
         let url2 = url.clone();
-        Box::new(self.client.get(url.parse().unwrap()).chain_err(move || {
-            format!("failed GET: {}", url)
-        }).and_then(|res| {
-            if res.status().is_success() {
-                let content_length = res.headers().get::<header::ContentLength>()
-                    .map(|&header::ContentLength(len)| len);
-                Ok((res.body(), content_length))
-            } else {
-                Err(ErrorKind::BadHTTPStatus(res.status().clone()).into())
-            }
-        }).and_then(|(body, content_length)| {
-            body.fold(Vec::new(), |mut body, chunk| {
-                body.extend_from_slice(&chunk);
-                Ok::<_, hyper::Error>(body)
-            }).chain_err(|| {
-                "failed to read HTTP body"
-            }).and_then(move |bytes| {
-                if let Some(len) = content_length {
-                    if len != bytes.len() as u64 {
-                        bail!(format!("Bad HTTP body size read: {}, expected {}", bytes.len(), len));
+        Box::new(
+            self.client
+                .get(&url[..])
+                .send()
+                .chain_err(move || format!("failed GET: {}", url))
+                .and_then(|res| {
+                    if res.status().is_success() {
+                        let content_length = res
+                            .headers()
+                            .get::<header::ContentLength>()
+                            .map(|&header::ContentLength(len)| len);
+                        Ok((res.into_body(), content_length))
                     } else {
-                        info!("Read {} bytes from {}", bytes.len(), url2);
+                        Err(ErrorKind::BadHTTPStatus(res.status().clone()).into())
                     }
-                }
-                Ok(bytes)
-            })
-        }))
+                })
+                .and_then(|(body, content_length)| {
+                    body.fold(Vec::new(), |mut body, chunk| {
+                        body.extend_from_slice(&chunk);
+                        Ok::<_, reqwest::Error>(body)
+                    }).chain_err(|| "failed to read HTTP body")
+                        .and_then(move |bytes| {
+                            if let Some(len) = content_length {
+                                if len != bytes.len() as u64 {
+                                    bail!(format!(
+                                        "Bad HTTP body size read: {}, expected {}",
+                                        bytes.len(),
+                                        len
+                                    ));
+                                } else {
+                                    info!("Read {} bytes from {}", bytes.len(), url2);
+                                }
+                            }
+                            Ok(bytes)
+                        })
+                }),
+        )
     }
 
-    pub fn put(&self, key: &str, content: Vec<u8>, creds: &AwsCredentials)
-               -> SFuture<()> {
+    pub fn put(&self, key: &str, content: Vec<u8>, creds: &AwsCredentials) -> SFuture<()> {
         let url = format!("{}{}", self.base_url, key);
         debug!("PUT {}", url);
         let mut request = Request::new(Method::Put, url.parse().unwrap());
@@ -122,55 +133,79 @@ impl Bucket {
         let mut canonical_headers = String::new();
         let token = creds.token().as_ref().map(|s| s.as_str());
         // Keep the list of header values sorted!
-        for (header, maybe_value) in vec![
-            ("x-amz-security-token", token),
-            ] {
+        for (header, maybe_value) in vec![("x-amz-security-token", token)] {
             if let Some(ref value) = maybe_value {
-                request.headers_mut()
-                       .set_raw(header, vec!(value.as_bytes().to_vec()));
-                canonical_headers.push_str(format!("{}:{}\n", header.to_ascii_lowercase(), value).as_ref());
+                request
+                    .headers_mut()
+                    .set_raw(header, vec![value.as_bytes().to_vec()]);
+                canonical_headers
+                    .push_str(format!("{}:{}\n", header.to_ascii_lowercase(), value).as_ref());
             }
         }
-        let auth = self.auth("PUT", &date, key, "", &canonical_headers, content_type, creds);
-        request.headers_mut().set_raw("Date", vec!(date.into_bytes()));
-        request.headers_mut().set(header::ContentType(content_type.parse().unwrap()));
-        request.headers_mut().set(header::ContentLength(content.len() as u64));
+        let auth = self.auth(
+            "PUT",
+            &date,
+            key,
+            "",
+            &canonical_headers,
+            content_type,
+            creds,
+        );
+        request
+            .headers_mut()
+            .set_raw("Date", vec![date.into_bytes()]);
+        request
+            .headers_mut()
+            .set(header::ContentType(content_type.parse().unwrap()));
+        request
+            .headers_mut()
+            .set(header::ContentLength(content.len() as u64));
         request.headers_mut().set(header::CacheControl(vec![
             // Two weeks
-            header::CacheDirective::MaxAge(1296000)
+            header::CacheDirective::MaxAge(1296000),
         ]));
-        request.headers_mut().set_raw("Authorization", vec!(auth.into_bytes()));
-        request.set_body(content);
+        request
+            .headers_mut()
+            .set_raw("Authorization", vec![auth.into_bytes()]);
+        *request.body_mut() = Some(content.into());
 
-        Box::new(self.client.request(request).then(|result| {
-            match result {
-                Ok(res) => {
-                    if res.status().is_success() {
-                        trace!("PUT succeeded");
-                        Ok(())
-                    } else {
-                        trace!("PUT failed with HTTP status: {}", res.status());
-                        Err(ErrorKind::BadHTTPStatus(res.status().clone()).into())
-                    }
+        Box::new(self.client.execute(request).then(|result| match result {
+            Ok(res) => {
+                if res.status().is_success() {
+                    trace!("PUT succeeded");
+                    Ok(())
+                } else {
+                    trace!("PUT failed with HTTP status: {}", res.status());
+                    Err(ErrorKind::BadHTTPStatus(res.status().clone()).into())
                 }
-                Err(e) => {
-                    trace!("PUT failed with error: {:?}", e);
-                    Err(e.into())
-                }
+            }
+            Err(e) => {
+                trace!("PUT failed with error: {:?}", e);
+                Err(e.into())
             }
         }))
     }
 
     // http://docs.aws.amazon.com/AmazonS3/latest/dev/RESTAuthentication.html
-    fn auth(&self, verb: &str, date: &str, path: &str,
-            md5: &str, headers: &str, content_type: &str, creds: &AwsCredentials) -> String {
-        let string = format!("{verb}\n{md5}\n{ty}\n{date}\n{headers}{resource}",
-                             verb = verb,
-                             md5 = md5,
-                             ty = content_type,
-                             date = date,
-                             headers = headers,
-                             resource = format!("/{}/{}", self.name, path));
+    fn auth(
+        &self,
+        verb: &str,
+        date: &str,
+        path: &str,
+        md5: &str,
+        headers: &str,
+        content_type: &str,
+        creds: &AwsCredentials,
+    ) -> String {
+        let string = format!(
+            "{verb}\n{md5}\n{ty}\n{date}\n{headers}{resource}",
+            verb = verb,
+            md5 = md5,
+            ty = content_type,
+            date = date,
+            headers = headers,
+            resource = format!("/{}/{}", self.name, path)
+        );
         let signature = signature(&string, creds.aws_secret_access_key());
         format!("AWS {}:{}", creds.aws_access_key_id(), signature)
     }
