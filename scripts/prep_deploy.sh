e70183f932f8f1e5a6d72e9a5f9fc8ffc9b6cfd8
Jake Shadle <jake.shadle@frostbite.com>
2019-02-11 12:13:53 +0100

    Fix checksum files

--- scripts/prep_deploy.sh
+++ scripts/prep_deploy.sh
@@ -5,5 +5,6 @@ mkdir $name
 cp target/$TARGET/release/sccache $name/
 cp README.md LICENSE $name/
 tar czvf $name.tar.gz $name
-chksum=($(shasum -ba 256 $name.tar.gz))
-echo "$chksum" > $name.tar.gz.sha256
+
+# Get the sha-256 checksum w/o filename and newline
+echo -n $(shasum -ba 256 "$name.tar.gz" | cut -d " " -f 1) > $name.tar.gz.sha256
