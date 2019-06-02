7698c94a373267971f89a3f2b5881d808cdb8ccd
Lzu Tao <taolzu@gmail.com>
2019-06-02 13:24:13 +0700

    Use ? operator instead of try! macro

--- lru-disk-cache/src/lib.rs
+++ lru-disk-cache/src/lib.rs
@@ -288,9 +288,9 @@ mod tests {
 
     fn create_file<T: AsRef<Path>, F: FnOnce(File) -> io::Result<()>>(dir: &Path, path: T, fill_contents: F) -> io::Result<PathBuf> {
         let b = dir.join(path);
-        r#try!(fs::create_dir_all(b.parent().unwrap()));
-        let f = r#try!(fs::File::create(&b));
-        r#try!(fill_contents(f));
+        fs::create_dir_all(b.parent().unwrap())?;
+        let f = fs::File::create(&b)?;
+        fill_contents(f)?;
         b.canonicalize()
     }
 
@@ -304,7 +304,7 @@ mod tests {
 
     fn read_all<R: Read>(r: &mut R) -> io::Result<Vec<u8>> {
         let mut v = vec!();
-        r#try!(r.read_to_end(&mut v));
+        r.read_to_end(&mut v)?;
         Ok(v)
     }
 
