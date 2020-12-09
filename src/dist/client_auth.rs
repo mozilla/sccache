46d51c1404684e5c051ed0cec8ff96410eff806f
Mike Hommey <mh@glandium.org>
2020-12-10 07:39:52 +0900

    Upgrade hmac to 0.10 and md-5, sha-1 and sha2 to 0.9

--- src/dist/client_auth.rs
+++ src/dist/client_auth.rs
@@ -190,8 +190,8 @@ mod code_grant_pkce {
         OsRng.fill_bytes(&mut code_verifier_bytes);
         let code_verifier = base64::encode_config(&code_verifier_bytes, base64::URL_SAFE_NO_PAD);
         let mut hasher = Sha256::new();
-        hasher.input(&code_verifier);
-        let code_challenge = base64::encode_config(&hasher.result(), base64::URL_SAFE_NO_PAD);
+        hasher.update(&code_verifier);
+        let code_challenge = base64::encode_config(&hasher.finalize(), base64::URL_SAFE_NO_PAD);
         Ok((code_verifier, code_challenge))
     }
 
