c4a3069eab7d0a441860aedd53efbbada3fc2774
Mike Hommey <mh@glandium.org>
2020-12-11 14:38:57 +0900

    Add a separate deployment build for the sccache-dist server

--- scripts/prep_deploy.sh
+++ scripts/prep_deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-name="sccache-$TRAVIS_TAG-$TARGET"
+name="sccache$SUFFIX-$TRAVIS_TAG-$TARGET"
 mkdir $name
-cp target/$TARGET/release/sccache $name/
+cp target/$TARGET/release/sccache$SUFFIX $name/
 cp README.md LICENSE $name/
 tar czvf $name.tar.gz $name
 
