edf0ac517650c7b230d933ec4a0947c103dd7647
Jonathan Schwender <55576758+jschwe@users.noreply.github.com>
2022-03-01 22:21:36 +0100

    Parse env-deps from dep-info (#1107)
    * Refactor sccache cargo test
    
    - Improve cleanup by using Result instead of panicking, to ensure that
    the temporary folder gets cleaned up.
    Previously, when a test failed, no clean up would happen. Now the cleanup
    happens, unless there is an unexpected panic somewhere in  the intergration test code.
    
    - this commit aims to make it easier to add new tests
    
    Split sccache cargo tests
    
    To make it clearer what is failing, split the cargo test into multiple
    tests. Since Sccache can't be invoked in parallel, we use the
    `serial_test`  to serialize testing,otherwise cargo by default would
    start them in parallel.
    
    The logger is now also lazily initialized, so the first test to run will
    initialize it. It now writes a linebreak, because otherwise the output
    is hard to read with
    `RUST_LOG=debug cargo test --test sccache_cargo -- --nocapture`.
    
    We catch panics (which are not intended by the tests ) to ensure that the
    temporary directory for the test gets cleaned up.
    
    Signed-off-by: Jonathan Schwender <schwenderjonathan@gmail.com>
    
    Remove panic=abort from CI coverage test
    
    I suspect the commandline was just taken like that from the example
    here: https://doc.rust-lang.org/stable/unstable-book/compiler-flags/profile.html
    
    However, there seems to be no reason to actually abort, since we don't have
    a custom test harness or anything.
    Since we want to catch panics, so we can correctly clean up, we need
    panic=unwind.
    
    Signed-off-by: Jonathan Schwender <schwenderjonathan@gmail.com>
    
    * Parse env_deps from dep-info
    
    This will trigger rebuilds if environment variables changed that the rust code
    depended on with env!.
    On the Rust side this requires at least Rust 1.46, otherwise there will be
    no env-dep info in the dep-info file. In that case we cannot detect
    the dependency on the env-value, and behaviour is unchanged compared to
    the current sccache behaviour.
    With recent Rust versions however, we can trigger a rebuild if a variable
    that is referenced via env! or option_env! is changed.
    
    Other env variables like `CARGO_*` or `RUSTFLAGS` which may affect
    the compilation are not listed in the dep-info file, so they have to
    be blanket added (as is currently already the case).
    
    Signed-off-by: Jonathan Schwender <schwenderjonathan@gmail.com>
    
    * Add test for cargo env_deps
    
    The previous commit added support for parsing "env_dep"
    information from dep_info files. This commit adds a test
    for changing an environment variable, that is referenced
    in rust code via env!, and asserts that sccache rebuilds
    and the rust code uses the new value of the changed
    environment variable.
    
    Signed-off-by: Jonathan Schwender <schwenderjonathan@gmail.com>

--- tests/sccache_cargo.rs
+++ tests/sccache_cargo.rs
@@ -3,135 +3,252 @@
 //! Any copyright is dedicated to the Public Domain.
 //! http://creativecommons.org/publicdomain/zero/1.0/
 
-#![deny(rust_2018_idioms)]
+use anyhow::{Context, Result};
+use once_cell::sync::Lazy;
 
-#[cfg(all(not(target_os = "windows"), not(target_os = "macos")))]
+use assert_cmd::prelude::*;
+use chrono::Local;
+use predicates::prelude::*;
+use serial_test::serial;
+use std::convert::Infallible;
+use std::ffi::OsString;
+use std::fs;
+use std::io::Write;
+use std::path::{Path, PathBuf};
+use std::process::{Command, Stdio};
+
 #[macro_use]
 extern crate log;
 
-/// Test that building a simple Rust crate with cargo using sccache results in a cache hit
-/// when built a second time.
-#[test]
-#[cfg(all(not(target_os = "windows"), not(target_os = "macos")))]
-fn test_rust_cargo() {
-    test_rust_cargo_cmd("check", &[]);
-    test_rust_cargo_cmd("build", &[]);
+static SCCACHE_BIN: Lazy<PathBuf> = Lazy::new(|| assert_cmd::cargo::cargo_bin("sccache"));
+static CARGO: Lazy<OsString> = Lazy::new(|| std::env::var_os("CARGO").unwrap());
+static CRATE_DIR: Lazy<PathBuf> =
+    Lazy::new(|| Path::new(file!()).parent().unwrap().join("test-crate"));
+/// Ensures the logger is only initialized once. Panics if initialization fails.
+static LOGGER: Lazy<Result<(), Infallible>> = Lazy::new(|| {
+    env_logger::Builder::new()
+        .format(|f, record| {
+            writeln!(
+                f,
+                "{} [{}] - {}",
+                Local::now().format("%Y-%m-%dT%H:%M:%S%.3f"),
+                record.level(),
+                record.args()
+            )
+        })
+        .parse_env("RUST_LOG")
+        .init();
+    Ok(())
+});
 
-    #[cfg(feature = "unstable")]
-    test_rust_cargo_cmd("check", &[("RUSTFLAGS", std::ffi::OsStr::new("-Zprofile"))]);
-    #[cfg(feature = "unstable")]
-    test_rust_cargo_cmd("build", &[("RUSTFLAGS", std::ffi::OsStr::new("-Zprofile"))]);
+/// Used as a test setup fixture. The drop implementation cleans up after a _successful_ test.
+/// We catch the panic to ensure that the drop runs and the TempDir is cleaned up.
+struct SccacheTest<'a> {
+    /// Tempdir used for Sccache cache and cargo output. It is kept in the struct only to have the
+    /// destructor run when SccacheTest goes out of scope, but is never used otherwise.
+    #[allow(dead_code)]
+    tempdir: tempfile::TempDir,
+    env: Vec<(&'a str, std::ffi::OsString)>,
 }
 
-#[cfg(all(not(target_os = "windows"), not(target_os = "macos")))]
-fn test_rust_cargo_cmd(cmd: &str, extra_envs: &[(&str, &std::ffi::OsStr)]) {
-    use assert_cmd::prelude::*;
-    use chrono::Local;
-    use predicates::prelude::*;
-    use std::env;
-    use std::ffi::OsStr;
-    use std::fs;
-    use std::io::Write;
-    use std::path::Path;
-    use std::process::{Command, Stdio};
+impl SccacheTest<'_> {
+    fn new(additional_envs: Option<&[(&'static str, std::ffi::OsString)]>) -> Result<Self> {
+        assert!(LOGGER.is_ok());
 
-    fn sccache_command() -> Command {
-        Command::new(assert_cmd::cargo::cargo_bin("sccache"))
+        // Create a temp directory to use for the disk cache.
+        let tempdir = tempfile::Builder::new()
+            .prefix("sccache_test_rust_cargo")
+            .tempdir()
+            .context("Failed to create tempdir")?;
+        let cache_dir = tempdir.path().join("cache");
+        fs::create_dir(&cache_dir)?;
+        let cargo_dir = tempdir.path().join("cargo");
+        fs::create_dir(&cargo_dir)?;
+
+        // Ensure there's no existing sccache server running.
+        stop_sccache()?;
+
+        trace!("sccache --start-server");
+
+        Command::new(SCCACHE_BIN.as_os_str())
+            .arg("--start-server")
+            .env("SCCACHE_DIR", &cache_dir)
+            .assert()
+            .try_success()
+            .context("Failed to start sccache server")?;
+
+        let mut env = vec![
+            ("CARGO_TARGET_DIR", cargo_dir.as_os_str().to_owned()),
+            ("RUSTC_WRAPPER", SCCACHE_BIN.as_os_str().to_owned()),
+            // Explicitly disable incremental compilation because sccache is unable to cache it at
+            // the time of writing.
+            ("CARGO_INCREMENTAL", OsString::from("0")),
+            ("TEST_ENV_VAR", OsString::from("1")),
+        ];
+
+        if let Some(vec) = additional_envs {
+            env.extend_from_slice(vec);
+        }
+
+        Ok(SccacheTest {
+            tempdir,
+            env: env.to_owned(),
+        })
     }
 
-    fn stop() {
-        trace!("sccache --stop-server");
-        drop(
-            sccache_command()
-                .arg("--stop-server")
-                .stdout(Stdio::null())
-                .stderr(Stdio::null())
-                .status(),
-        );
+    /// Show the statistics for sccache. This will be called at the end of a test and making this
+    /// an associated function will ensure that the struct lives until the end of the test.
+    fn show_stats(&self) -> assert_cmd::assert::AssertResult {
+        trace!("sccache --show-stats");
+        Command::new(SCCACHE_BIN.as_os_str())
+            .args(&["--show-stats", "--stats-format=json"])
+            .assert()
+            .try_success()
     }
+}
 
-    drop(
-        env_logger::Builder::new()
-            .format(|f, record| {
-                write!(
-                    f,
-                    "{} [{}] - {}",
-                    Local::now().format("%Y-%m-%dT%H:%M:%S%.3f"),
-                    record.level(),
-                    record.args()
-                )
-            })
-            .parse_env("RUST_LOG")
-            .try_init(),
-    );
-    let cargo = env!("CARGO");
-    debug!("cargo: {}", cargo);
-    let sccache = assert_cmd::cargo::cargo_bin("sccache");
-    debug!("sccache: {:?}", sccache);
-    let crate_dir = Path::new(file!()).parent().unwrap().join("test-crate");
-    // Ensure there's no existing sccache server running.
-    stop();
-    // Create a temp directory to use for the disk cache.
-    let tempdir = tempfile::Builder::new()
-        .prefix("sccache_test_rust_cargo")
-        .tempdir()
-        .unwrap();
-    let cache_dir = tempdir.path().join("cache");
-    fs::create_dir(&cache_dir).unwrap();
-    let cargo_dir = tempdir.path().join("cargo");
-    fs::create_dir(&cargo_dir).unwrap();
-    // Start a new sccache server.
-    trace!("sccache --start-server");
-    sccache_command()
-        .arg("--start-server")
-        .env("SCCACHE_DIR", &cache_dir)
-        .assert()
-        .success();
+impl Drop for SccacheTest<'_> {
+    fn drop(&mut self) {
+        stop_sccache().expect("Stopping Sccache server failed");
+    }
+}
+
+fn stop_sccache() -> Result<()> {
+    trace!("sccache --stop-server");
+
+    Command::new(SCCACHE_BIN.as_os_str())
+        .arg("--stop-server")
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .status()
+        .context("Failed to stop sccache server")?;
+    Ok(())
+}
+
+#[test]
+#[serial]
+fn test_rust_cargo_check() -> Result<()> {
+    test_rust_cargo_cmd("check", SccacheTest::new(None)?)
+}
+
+#[test]
+#[serial]
+fn test_rust_cargo_build() -> Result<()> {
+    test_rust_cargo_cmd("build", SccacheTest::new(None)?)
+}
+
+/// This test checks that changing an environment variable reference by env! is detected by
+/// sccache, causes a rebuild and is correctly printed to stdout.
+#[test]
+#[serial]
+fn test_rust_cargo_run_with_env_dep_parsing() -> Result<()> {
+    test_rust_cargo_env_dep(SccacheTest::new(None)?)
+}
+
+#[cfg(feature = "unstable")]
+#[test]
+#[serial]
+fn test_rust_cargo_check_nightly() -> Result<()> {
+    test_rust_cargo_cmd(
+        "check",
+        SccacheTest::new(Some(&[("RUSTFLAGS", OsString::from("-Zprofile"))]))?,
+    )
+}
+
+#[cfg(feature = "unstable")]
+#[test]
+#[serial]
+fn test_rust_cargo_build_nightly() -> Result<()> {
+    test_rust_cargo_cmd(
+        "build",
+        SccacheTest::new(Some(&[("RUSTFLAGS", OsString::from("-Zprofile"))]))?,
+    )
+}
+
+/// Test that building a simple Rust crate with cargo using sccache results in a cache hit
+/// when built a second time and a cache miss, when the environment variable referenced via
+/// env! is changed.
+fn test_rust_cargo_cmd(cmd: &str, test_info: SccacheTest) -> Result<()> {
     // `cargo clean` first, just to be sure there's no leftover build objects.
-    let mut envs = vec![
-        ("RUSTC_WRAPPER", sccache.as_ref()),
-        ("CARGO_TARGET_DIR", cargo_dir.as_ref()),
-        // Explicitly disable incremental compilation because sccache is unable
-        // to cache it at the time of writing.
-        ("CARGO_INCREMENTAL", OsStr::new("0")),
-    ];
-    envs.extend_from_slice(extra_envs);
-    Command::new(&cargo)
+    Command::new(CARGO.as_os_str())
         .args(&["clean"])
-        .envs(envs.iter().copied())
-        .current_dir(&crate_dir)
+        .envs(test_info.env.iter().cloned())
+        .current_dir(CRATE_DIR.as_os_str())
         .assert()
-        .success();
+        .try_success()?;
     // Now build the crate with cargo.
-    Command::new(&cargo)
+    Command::new(CARGO.as_os_str())
         .args(&[cmd, "--color=never"])
-        .envs(envs.iter().copied())
-        .current_dir(&crate_dir)
+        .envs(test_info.env.iter().cloned())
+        .current_dir(CRATE_DIR.as_os_str())
         .assert()
-        .stderr(predicates::str::contains("\x1b[").from_utf8().not())
-        .success();
+        .try_stderr(predicates::str::contains("\x1b[").from_utf8().not())?
+        .try_success()?;
     // Clean it so we can build it again.
-    Command::new(&cargo)
+    Command::new(CARGO.as_os_str())
         .args(&["clean"])
-        .envs(envs.iter().copied())
-        .current_dir(&crate_dir)
+        .envs(test_info.env.iter().cloned())
+        .current_dir(CRATE_DIR.as_os_str())
         .assert()
-        .success();
-    Command::new(&cargo)
+        .try_success()?;
+    Command::new(CARGO.as_os_str())
         .args(&[cmd, "--color=always"])
-        .envs(envs.iter().copied())
-        .current_dir(&crate_dir)
+        .envs(test_info.env.iter().cloned())
+        .current_dir(CRATE_DIR.as_os_str())
         .assert()
-        .stderr(predicates::str::contains("\x1b[").from_utf8())
-        .success();
-    // Now get the stats and ensure that we had a cache hit for the second build.
-    // The test crate has one dependency (itoa) so there are two separate
-    // compilations.
-    trace!("sccache --show-stats");
-    sccache_command()
-        .args(&["--show-stats", "--stats-format=json"])
+        .try_stderr(predicates::str::contains("\x1b[").from_utf8())?
+        .try_success()?;
+
+    test_info
+        .show_stats()?
+        .try_stdout(predicates::str::contains(r#""cache_hits":{"counts":{"Rust":2}}"#).from_utf8())?
+        .try_success()?;
+
+    Ok(())
+}
+
+fn test_rust_cargo_env_dep(test_info: SccacheTest) -> Result<()> {
+    // `cargo clean` first, just to be sure there's no leftover build objects.
+    Command::new(CARGO.as_os_str())
+        .args(&["clean"])
+        .envs(test_info.env.iter().cloned())
+        .current_dir(CRATE_DIR.as_os_str())
         .assert()
-        .stdout(predicates::str::contains(r#""cache_hits":{"counts":{"Rust":2}}"#).from_utf8())
-        .success();
-    stop();
+        .try_success()?;
+    // Now build the crate with cargo.
+    Command::new(CARGO.as_os_str())
+        .args(&["run", "--color=never"])
+        .envs(test_info.env.iter().cloned())
+        .current_dir(CRATE_DIR.as_os_str())
+        .assert()
+        .try_stderr(predicates::str::contains("\x1b[").from_utf8().not())?
+        .try_stdout(predicates::str::contains("Env var: 1"))?
+        .try_success()?;
+    // Clean it so we can build it again.
+    Command::new(CARGO.as_os_str())
+        .args(&["clean"])
+        .envs(test_info.env.iter().cloned())
+        .current_dir(CRATE_DIR.as_os_str())
+        .assert()
+        .try_success()?;
+    Command::new(CARGO.as_os_str())
+        .args(&["run", "--color=always"])
+        .envs(test_info.env.iter().cloned())
+        .env("TEST_ENV_VAR", "OTHER_VALUE")
+        .current_dir(CRATE_DIR.as_os_str())
+        .assert()
+        .try_stderr(predicates::str::contains("\x1b[").from_utf8())?
+        .try_stdout(predicates::str::contains("Env var: OTHER_VALUE"))?
+        .try_success()?;
+
+    // Now get the stats and ensure that we had one cache hit for the second build.
+    // The test crate has one dependency (itoa) so there are two separate compilations, but only
+    // itoa should be cached (due to the changed environment variable).
+    test_info
+        .show_stats()?
+        .try_stdout(predicates::str::contains(r#""cache_hits":{"counts":{"Rust":1}}"#).from_utf8())?
+        .try_success()?;
+
+    drop(test_info);
+    Ok(())
 }
