360fc0127afdd10377e65cfd5ea92c6c6e4e084c
figsoda <figsoda@pm.me>
2022-12-03 14:51:49 -0500

    fix https://github.com/mozilla/sccache/issues/1437

---
+++ tests/test_clang_multicall.c
@@ -0,0 +1,9 @@
+#include <iostream>
+
+// this is c++ code, but the extension is .c,
+// so clang doesn't change it's behavior because of the extension
+
+int main() {
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}
