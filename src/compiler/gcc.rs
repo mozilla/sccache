160ccd8192a8535d23ae402c34ded4c391bf3b47
Daniel <yimingdz@gmail.com>
2022-12-26 22:26:47 +0800

    fix: handle -Xclang args when invoking clang/clang++ on windows cmake close #955

--- src/compiler/gcc.rs
+++ src/compiler/gcc.rs
@@ -436,8 +436,8 @@ where
             None => match arg {
                 Argument::Raw(_) if follows_plugin_arg => &mut common_args,
                 Argument::Raw(_) => cannot_cache!("Can't handle Raw arguments with -Xclang"),
-                Argument::UnknownFlag(_) => {
-                    cannot_cache!("Can't handle UnknownFlag arguments with -Xclang")
+                Argument::UnknownFlag(flag) => {
+                    cannot_cache!("Can't handle UnknownFlag arguments with -Xclang", flag.to_str().unwrap_or("").to_string())
                 }
                 _ => unreachable!(),
             },
