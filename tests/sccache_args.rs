08fac54e3d51d658c3f574e77a90869f0f2183c7
Xuanwo <github@xuanwo.io>
2023-01-02 20:11:28 +0800

    feat: Implement cache storage check
    Signed-off-by: Xuanwo <github@xuanwo.io>

---
+++ tests/sccache_args.rs
@@ -0,0 +1,88 @@
+//! Tests for sccache args.
+//!
+//! Any copyright is dedicated to the Public Domain.
+//! http://creativecommons.org/publicdomain/zero/1.0/
+
+use anyhow::{Context, Result};
+use assert_cmd::prelude::*;
+use once_cell::sync::Lazy;
+use predicates::prelude::*;
+use serial_test::serial;
+use std::path::PathBuf;
+use std::process::{Command, Stdio};
+
+#[macro_use]
+extern crate log;
+
+static SCCACHE_BIN: Lazy<PathBuf> = Lazy::new(|| assert_cmd::cargo::cargo_bin("sccache"));
+
+fn stop_sccache() -> Result<()> {
+    trace!("sccache --stop-server");
+
+    Command::new(SCCACHE_BIN.as_os_str())
+        .arg("--stop-server")
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .status()
+        .context("Failed to stop sccache server")?;
+    Ok(())
+}
+
+#[test]
+#[serial]
+fn test_gcp_arg_check() -> Result<()> {
+    trace!("sccache with log");
+    stop_sccache()?;
+
+    let mut cmd = Command::new(SCCACHE_BIN.as_os_str());
+    cmd.arg("--start-server")
+        .env("SCCACHE_LOG", "debug")
+        .env("SCCACHE_GCS_KEY_PATH", "foo.json");
+
+    cmd.assert().failure().stderr(predicate::str::contains(
+        "If setting GCS credentials, SCCACHE_GCS_BUCKET",
+    ));
+
+    stop_sccache()?;
+
+    let mut cmd = Command::new(SCCACHE_BIN.as_os_str());
+    cmd.arg("--start-server")
+        .env("SCCACHE_LOG", "debug")
+        .env("SCCACHE_GCS_OAUTH_URL", "http://127.0.0.1");
+
+    cmd.assert().failure().stderr(predicate::str::contains(
+        "If setting GCS credentials, SCCACHE_GCS_BUCKET",
+    ));
+
+    stop_sccache()?;
+    let mut cmd = Command::new(SCCACHE_BIN.as_os_str());
+    cmd.arg("--start-server")
+        .env("SCCACHE_LOG", "debug")
+        .env("SCCACHE_GCS_BUCKET", "b")
+        .env("SCCACHE_GCS_CREDENTIALS_URL", "not_valid_url//127.0.0.1")
+        .env("SCCACHE_GCS_KEY_PATH", "foo.json");
+
+    // This is just a warning
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("gcs credential url is invalid"));
+
+    Ok(())
+}
+
+#[test]
+#[serial]
+fn test_s3_invalid_args() -> Result<()> {
+    stop_sccache()?;
+
+    let mut cmd = Command::new(SCCACHE_BIN.as_os_str());
+    cmd.arg("--start-server")
+        .env("SCCACHE_LOG", "debug")
+        .env("SCCACHE_BUCKET", "test");
+
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("cache storage failed to read"));
+
+    Ok(())
+}
