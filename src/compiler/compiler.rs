a426f30d5d04a00b0b28e9284e02e64f803dc7f3
Sylvestre Ledru <sledru@mozilla.com>
2023-02-22 14:15:45 +0100

    use fs_err
    Co-authored-by: Bernhard Schuster <bernhard@ahoi.io>

--- src/compiler/compiler.rs
+++ src/compiler/compiler.rs
@@ -37,7 +37,7 @@ use std::borrow::Cow;
 use std::ffi::{OsStr, OsString};
 use std::fmt;
 #[cfg(not(feature = "dist-client"))]
-use std::fs;
+use fs_err as fs;
 use std::future::Future;
 use std::io::prelude::*;
 use std::path::{Path, PathBuf};
