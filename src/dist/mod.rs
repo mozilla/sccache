8742d91acf213f76d2a7a45b41c91ebed7d7d5ad
Sylvestre Ledru <sledru@mozilla.com>
2023-03-27 11:39:58 +0200

    Revert "Support running dist compilations in unprivileged scenarios (#128)"
    This reverts commit 20a08fc079c6afe09ef6d82edccda6ca5273c8a1.

--- src/dist/mod.rs
+++ src/dist/mod.rs
@@ -593,7 +593,6 @@ pub enum SubmitToolchainResult {
 
 // BuildResult
 
-#[derive(Serialize, Deserialize)]
 pub struct BuildResult {
     pub output: ProcessOutput,
     pub outputs: Vec<(String, OutputData)>,
