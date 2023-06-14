c2990570e4d833af8d1002e046a4e06ccb81b484
t_max <1172915550@qq.com>
2023-06-14 16:04:45 +0800

    enh(msvc): ignore /d2Zi+
    Signed-off-by: t_max <1172915550@qq.com>

--- src/compiler/msvc.rs
+++ src/compiler/msvc.rs
@@ -481,6 +481,7 @@ msvc_args!(static ARGS: [ArgInfo<ArgData>; _] = [
     msvc_flag!("clr", PassThrough),
     msvc_take_arg!("clr:", OsString, Concatenated, PassThroughWithSuffix),
     msvc_take_arg!("constexpr:", OsString, Concatenated, PassThroughWithSuffix),
+    msvc_flag!("d2Zi+", PassThrough),
     msvc_take_arg!("deps", PathBuf, Concatenated, DepFile),
     msvc_take_arg!("diagnostics:", OsString, Concatenated, PassThroughWithSuffix),
     msvc_take_arg!("doc", PathBuf, Concatenated, TooHardPath), // Creates an .xdc file.
