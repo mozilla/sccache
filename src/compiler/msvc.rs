d7e7b5bf748932ac178a1cb3e75b964972fd88fe
Audran Doublet <audran.doublet@sinequa.com>
2023-07-21 16:40:15 +0200

    msvc: fix detect_showincludes_prefix when VS_UNICODE_OUTPUT is set

--- src/compiler/msvc.rs
+++ src/compiler/msvc.rs
@@ -201,13 +201,9 @@ where
     if is_clang {
         cmd.arg("--driver-mode=cl");
     }
-    cmd.args(&["-nologo", "-showIncludes", "-c", "-Fonul", "-I."])
+    cmd.args(&["-nologo", "-showIncludes", "-c", "-Fonul", "-I.", "-E"])
         .arg(&input)
-        .current_dir(tempdir.path())
-        // The MSDN docs say the -showIncludes output goes to stderr,
-        // but that's not true unless running with -E.
-        .stdout(Stdio::piped())
-        .stderr(Stdio::null());
+        .current_dir(tempdir.path());
     for (k, v) in env {
         cmd.env(k, v);
     }
@@ -220,12 +216,13 @@ where
     }
 
     let process::Output {
-        stdout: stdout_bytes,
+        stderr: stderr_bytes,
         ..
     } = output;
-    let stdout = from_local_codepage(&stdout_bytes)
-        .context("Failed to convert compiler stdout while detecting showIncludes prefix")?;
-    for line in stdout.lines() {
+    let output = from_local_codepage(&stderr_bytes)
+        .context("Failed to convert compiler stderr while detecting showIncludes prefix")?;
+
+    for line in output.lines() {
         if !line.ends_with("test.h") {
             continue;
         }
@@ -246,7 +243,7 @@ where
 
     debug!(
         "failed to detect showIncludes prefix with output: {}",
-        stdout
+        output
     );
 
     bail!("Failed to detect showIncludes prefix")
