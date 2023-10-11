6b6bebb60ed132d52abf79dd2663a395b1f85180
Raphaël Gomès <rgomes@octobus.net>
2023-10-11 15:37:34 +0200

    Add utils to scan for C time macros while hashing
    This will be useful to handle time macros properly during caching
    preprocessor output in a future commit, while also not reading the
    contents twice.

--- src/util.rs
+++ src/util.rs
@@ -19,6 +19,7 @@ use fs::File;
 use fs_err as fs;
 use object::{macho, read::archive::ArchiveFile, read::macho::FatArch};
 use serde::{Deserialize, Serialize};
+use std::cell::Cell;
 use std::ffi::{OsStr, OsString};
 use std::hash::Hasher;
 use std::io::prelude::*;
@@ -33,6 +34,8 @@ use crate::errors::*;
 /// The url safe engine for base64.
 pub const BASE64_URL_SAFE_ENGINE: base64::engine::GeneralPurpose =
     base64::engine::general_purpose::URL_SAFE_NO_PAD;
+
+pub const HASH_BUFFER_SIZE: usize = 128 * 1024;
 
 #[derive(Clone)]
 pub struct Digest {
@@ -56,19 +59,38 @@ impl Digest {
     }
 
     /// Calculate the BLAKE3 digest of the contents read from `reader`.
-    pub fn reader_sync<R: Read>(mut reader: R) -> Result<String> {
+    pub fn reader_sync<R: Read>(reader: R) -> Result<String> {
+        Self::reader_sync_with(reader, |_| {}).map(|d| d.finish())
+    }
+
+    /// Calculate the BLAKE3 digest of the contents read from `reader`, calling
+    /// `each` before each time the digest is updated.
+    pub fn reader_sync_with<R: Read, F: FnMut(&[u8])>(mut reader: R, mut each: F) -> Result<Self> {
         let mut m = Digest::new();
         // A buffer of 128KB should give us the best performance.
         // See https://eklitzke.org/efficient-file-copying-on-linux.
-        let mut buffer = [0; 128 * 1024];
+        let mut buffer = [0; HASH_BUFFER_SIZE];
         loop {
             let count = reader.read(&mut buffer[..])?;
             if count == 0 {
                 break;
             }
+            each(&buffer[..count]);
             m.update(&buffer[..count]);
         }
-        Ok(m.finish())
+        Ok(m)
+    }
+
+    /// Calculate the BLAKE3 digest of the contents read from `reader`, while
+    /// also checking for the presence of time macros.
+    /// See [`TimeMacroFinder`] for more details.
+    pub fn reader_sync_time_macros<R: Read>(reader: R) -> Result<(String, TimeMacroFinder)> {
+        let mut finder = TimeMacroFinder::new();
+
+        Ok((
+            Self::reader_sync_with(reader, |visit| finder.find_time_macros(visit))?.finish(),
+            finder,
+        ))
     }
 
     /// Calculate the BLAKE3 digest of the contents of `path`, running
@@ -94,6 +116,172 @@ impl Digest {
 impl Default for Digest {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+/// The longest pattern we're looking for is `__TIMESTAMP__`
+const MAX_HAYSTACK_LEN: usize = b"__TIMESTAMP__".len();
+
+#[cfg(test)]
+pub const MAX_TIME_MACRO_HAYSTACK_LEN: usize = MAX_HAYSTACK_LEN;
+
+/// Used during the chunked hashing process to check for C preprocessor time
+/// macros (namely `__TIMESTAMP__`, `__DATE__`, `__DATETIME__`) while reusing
+/// the same buffer as the hashing function, for efficiency.
+///
+/// See `[Self::find_time_macros]` for details.
+#[derive(Debug, Default)]
+pub struct TimeMacroFinder {
+    pub found_date: Cell<bool>,
+    pub found_time: Cell<bool>,
+    pub found_timestamp: Cell<bool>,
+    overlap_buffer: [u8; MAX_HAYSTACK_LEN * 2],
+    /// Counter of chunks of full size we've been through. Partial reads do
+    /// not count and are handled separately.
+    full_chunks_counter: usize,
+    /// Contents of the previous read if it was smaller than `MAX_HAYSTACK_LEN`,
+    /// plus MAX_HAYSTACK_LEN bytes of the previous chunk, to account for
+    /// the possibility of partial reads splitting a time macro
+    /// across two calls.
+    previous_small_read: Vec<u8>,
+}
+
+impl TimeMacroFinder {
+    /// Called for each chunk of a file during the hashing process
+    /// in preprocessor cache mode.
+    ///
+    /// When buffer reading a file, we get something like this:
+    ///
+    /// `[xxxx....aaaa][bbbb....cccc][dddd....eeee][ffff...]`
+    ///
+    /// The brackets represent each buffer chunk. We use the fact that the largest
+    /// pattern we're looking for is `__TIMESTAMP__` to avoid copying the entire
+    /// file to memory and re-searching the entire buffer for each pattern.
+    /// We can check inside each chunk for each pattern, and we use an overlap
+    /// buffer to keep the last `b"__TIMESTAMP__".len()` bytes around from the
+    /// last chunk, to also catch any pattern overlapping two chunks.
+    ///
+    /// In the above case, the overflow buffer would look like:
+    ///
+    /// ```text
+    ///    Chunk 1
+    ///    - aaaa0000
+    ///    Chunk 2
+    ///    - aaaabbbb
+    ///    - cccc0000
+    ///    Chunk 3
+    ///    - ccccdddd
+    ///    - eeee0000
+    ///    Chunk 4
+    ///    - eeeeffff
+    ///    [...]
+    /// ```
+    ///
+    /// We have to be careful to zero out the buffer right after each overlap check,
+    /// otherwise we risk the (unlikely) case of a pattern being spread between the
+    /// start of a chunk and its end.
+    /// Finally, we need to account for partial reads: it's possible that a read
+    /// smaller than the haystack hide a time macro because it spreads it across
+    /// two calls. This makes the example more complicated and isn't necessary
+    /// to get the point of the algorithm across.
+    /// See unit tests for some concrete examples.
+    pub fn find_time_macros(&mut self, visit: &[u8]) {
+        if self.full_chunks_counter == 0 {
+            if visit.len() <= MAX_HAYSTACK_LEN {
+                // The read is smaller than the largest haystack.
+                // We might get called again, if this was an incomplete read.
+                if !self.previous_small_read.is_empty() {
+                    // In a rare pathological case where all reads are small,
+                    // this will grow up to the length of the file.
+                    // It it *very* unlikely and of minor performance
+                    // importance compared to just getting many small reads.
+                    self.previous_small_read.extend(visit);
+                } else {
+                    self.previous_small_read = visit.to_owned();
+                }
+                self.find_macros(&self.previous_small_read);
+                return;
+            }
+            // Copy the right side of the visit to the left of the buffer
+            let right_half = visit.len() - MAX_HAYSTACK_LEN;
+            self.overlap_buffer[..MAX_HAYSTACK_LEN].copy_from_slice(&visit[right_half..]);
+        } else {
+            if visit.len() < MAX_HAYSTACK_LEN {
+                // The read is smaller than the largest haystack.
+                // We might get called again, if this was an incomplete read.
+                if !self.previous_small_read.is_empty() {
+                    self.previous_small_read.extend(visit);
+                } else {
+                    // Since this isn't the first non-small read (counter != 0)
+                    // we need to start from MAX_HAYSTACK_LEN bytes of the previous
+                    // read, otherwise we might miss a complete read followed
+                    // by a small read.
+                    let mut buf = self.overlap_buffer[..MAX_HAYSTACK_LEN].to_owned();
+                    buf.extend(visit);
+                    self.previous_small_read = buf;
+                }
+
+                // zero the right side of the buffer
+                self.overlap_buffer[MAX_HAYSTACK_LEN..].copy_from_slice(&[0; MAX_HAYSTACK_LEN]);
+                // Copy the visit to the right of the buffer, starting from the middle
+                self.overlap_buffer[MAX_HAYSTACK_LEN..MAX_HAYSTACK_LEN + visit.len()]
+                    .copy_from_slice(visit);
+
+                // Check both the concatenation with the previous small read
+                self.find_macros(&self.previous_small_read);
+                // ...and the overlap buffer
+                self.find_macros(&self.overlap_buffer);
+                return;
+            } else {
+                // Copy the left side of the visit to the right of the buffer
+                let left_half = MAX_HAYSTACK_LEN;
+                self.overlap_buffer[left_half..].copy_from_slice(&visit[..left_half]);
+                self.find_macros(&self.overlap_buffer);
+                // zero the buffer
+                self.overlap_buffer = Default::default();
+                // Copy the right side of the visit to the left of the buffer
+                let right_half = visit.len() - MAX_HAYSTACK_LEN;
+                self.overlap_buffer[..MAX_HAYSTACK_LEN].copy_from_slice(&visit[right_half..]);
+            }
+            self.find_macros(&self.overlap_buffer);
+        }
+        // Also check the concatenation with the previous small read
+        if !self.previous_small_read.is_empty() {
+            let mut concatenated = self.previous_small_read.to_owned();
+            concatenated.extend(visit);
+            self.find_macros(&concatenated);
+        }
+
+        self.find_macros(visit);
+        self.full_chunks_counter += 1;
+        self.previous_small_read.clear();
+    }
+
+    fn find_macros(&self, buffer: &[u8]) {
+        // TODO
+        // This could be made more efficient, either by using a regex for all
+        // three patterns, or by doing some SIMD trickery like `ccache` does.
+        //
+        // `ccache` reads the file twice, so we might actually already be
+        // winning in most cases... though they have an inode cache.
+        // In any case, let's only improve this if it ends up being slow.
+        if memchr::memmem::find(buffer, b"__TIMESTAMP__").is_some() {
+            self.found_timestamp.set(true);
+        }
+        if memchr::memmem::find(buffer, b"__TIME__").is_some() {
+            self.found_time.set(true);
+        };
+        if memchr::memmem::find(buffer, b"__DATE__").is_some() {
+            self.found_date.set(true);
+        };
+    }
+
+    pub fn found_time_macros(&self) -> bool {
+        self.found_date.get() || self.found_time.get() || self.found_timestamp.get()
+    }
+
+    pub fn new() -> Self {
+        Default::default()
     }
 }
 
