72c1542e6958a61a538b0ece6a783771045fa267
Mike Hommey <mh@glandium.org>
2023-11-14 12:54:39 +0900

    Comment out the logging setup in the test harness
    It was accidentally uncommented in 520a30b6.

--- tests/harness/mod.rs
+++ tests/harness/mod.rs
@@ -46,11 +46,11 @@ pub fn start_local_daemon(cfg_path: &Path, cached_cfg_path: &Path) {
     if !sccache_command()
         .arg("--start-server")
         // Uncomment following lines to debug locally.
-        .env("SCCACHE_LOG", "debug")
-        .env(
-            "SCCACHE_ERROR_LOG",
-            env::temp_dir().join("sccache_local_daemon.txt"),
-        )
+        // .env("SCCACHE_LOG", "debug")
+        // .env(
+        //     "SCCACHE_ERROR_LOG",
+        //     env::temp_dir().join("sccache_local_daemon.txt"),
+        // )
         .env("SCCACHE_CONF", cfg_path)
         .env("SCCACHE_CACHED_CONF", cached_cfg_path)
         .status()
