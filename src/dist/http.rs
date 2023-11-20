bbada425d7e9b0f9fcb1151fe6c2b273d87110c7
Jiahao XU <Jiahao_XU@outlook.com>
2023-11-20 23:54:50 +1100

    dep: Upgrade trust-dns-resolver v0.22 to hickory-resolver v0.24
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/dist/http.rs
+++ src/dist/http.rs
@@ -1087,6 +1087,7 @@ mod client {
         self, AllocJobResult, CompileCommand, JobAlloc, PathTransformer, RunJobResult,
         SchedulerStatusResult, SubmitToolchainResult, Toolchain,
     };
+    use crate::util::HickoryResolver;
 
     use async_trait::async_trait;
     use byteorder::{BigEndian, WriteBytesExt};
@@ -1139,6 +1140,7 @@ mod client {
                 // Disable connection pool to avoid broken connection
                 // between runtime
                 .pool_max_idle_per_host(0)
+                .dns_resolver(Arc::new(HickoryResolver::default()))
                 .build()
                 .context("failed to create an async HTTP client")?;
             let client_toolchains =
