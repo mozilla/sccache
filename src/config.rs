7ce78106107deb250aa7c6519e70adeb3284ffe9
Mike Hommey <mh@glandium.org>
2023-11-23 16:31:03 +0900

    Remove uses of Lazy<Mutex<T>>
    Mutex<T>::new has been const since 1.63.0.

--- src/config.rs
+++ src/config.rs
@@ -37,7 +37,7 @@ pub use crate::cache::PreprocessorCacheModeConfig;
 use crate::errors::*;
 
 static CACHED_CONFIG_PATH: Lazy<PathBuf> = Lazy::new(CachedConfig::file_config_path);
-static CACHED_CONFIG: Lazy<Mutex<Option<CachedFileConfig>>> = Lazy::new(|| Mutex::new(None));
+static CACHED_CONFIG: Mutex<Option<CachedFileConfig>> = Mutex::new(None);
 
 const ORGANIZATION: &str = "Mozilla";
 const APP_NAME: &str = "sccache";
