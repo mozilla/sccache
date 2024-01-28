e2345d03ffae66bc571ffad0961456e6223bd492
Robert Dupuis <robert.dupuis@cspi.com>
2024-01-28 01:38:48 +0000

    Try getting sccache to work with OpenSSL.

--- src/compiler/msvc.rs
+++ src/compiler/msvc.rs
@@ -417,8 +417,8 @@ msvc_args!(static ARGS: [ArgInfo<ArgData>; _] = [
     msvc_flag!("clr", PassThrough),
     msvc_take_arg!("clr:", OsString, Concatenated, PassThroughWithSuffix),
     msvc_take_arg!("constexpr:", OsString, Concatenated, PassThroughWithSuffix),
-    msvc_take_arg!("d1nodatetime", PassThrough),
-    msvc_take_arg!("d1trimfile", PassThrough),
+    msvc_flag!("d1nodatetime", PassThrough),
+    msvc_take_arg!("d1trimfile:", PathBuf, Concatenated, PreprocessorArgumentPath),
     msvc_take_arg!("deps", PathBuf, Concatenated, DepFile),
     msvc_take_arg!("diagnostics:", OsString, Concatenated, PassThroughWithSuffix),
     msvc_take_arg!("doc", PathBuf, Concatenated, TooHardPath), // Creates an .xdc file.
