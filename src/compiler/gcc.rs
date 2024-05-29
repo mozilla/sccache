df191d69a2587db1e6d7955facd736c00af5fa89
Mike Hommey <mh@glandium.org>
2024-05-29 13:28:17 +0900

    Disable preprocessor cache with -MD and friends
    Also hook up too_hard_for_preprocessor_cache_mode, because it wasn't.

--- src/compiler/gcc.rs
+++ src/compiler/gcc.rs
@@ -350,6 +350,7 @@ where
             }
             Some(Output(p)) => output_arg = Some(p.clone()),
             Some(NeedDepTarget) => {
+                too_hard_for_preprocessor_cache_mode = true;
                 need_explicit_dep_target = true;
                 if let DepArgumentRequirePath::NotNeeded = need_explicit_dep_argument_path {
                     need_explicit_dep_argument_path = DepArgumentRequirePath::Missing;
