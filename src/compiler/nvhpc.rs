2debbfccf627437a6d3d4d049568f9cfb6e42c1e
Mikko Lehtonen <scoopr@iki.fi>
2024-10-02 10:46:21 +0300

    Implement features required by Xcode (#2257)
    * Mark `-index-store-path` as Hard
    
    Xcode sets it by default, but it can be disabled with
    `COMPILER_INDEX_STORE_ENABLE=NO` Xcode build setting.
    
    * Mark serialize-diagnostics and deps as artifacts
    
    Xcode requires these files to be present after compiling, so they need
    to be stored and restored for the build to complete.
    
    * Update tests for the dep changes
    
    * Add test for `-index-store-path`
    
    * Add documentation for Xcode use
    
    * Xcode integration test
    
    * Add comment about wrapper script use

--- src/compiler/nvhpc.rs
+++ src/compiler/nvhpc.rs
@@ -340,6 +340,13 @@ mod test {
                     path: "foo.o".into(),
                     optional: false
                 }
+            ),
+            (
+                "d",
+                ArtifactDescriptor {
+                    path: "foo.o.d".into(),
+                    optional: false
+                }
             )
         );
         assert_eq!(
