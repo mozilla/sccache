4e4ca502a03ac6936a1fcad098e84291c59e728e
Aditya-PS-05 <adityapratapsjnhh7654@gmail.com>
2024-11-12 11:50:26 +0530

    fixed --no-default-features --features=gcs warnings

--- src/compiler/rust.rs
+++ src/compiler/rust.rs
@@ -172,6 +172,7 @@ pub struct ParsedArguments {
 
 /// A struct on which to hang a `Compilation` impl.
 #[derive(Debug, Clone)]
+#[allow(dead_code)]
 pub struct RustCompilation {
     /// The path to the rustc executable, not the rustup proxy.
     executable: PathBuf,
