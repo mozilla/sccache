bcddc254ab4c4d2649c6713822f3ebd7ae0b32d2
Troy Benson <troybensonsa@gmail.com>
2025-12-22 15:17:14 -0700

    fix fmt

--- src/compiler/gcc.rs
+++ src/compiler/gcc.rs
@@ -395,7 +395,10 @@ where
                 } else if p.is_empty() {
                     module_output_path = Some(None);
                 } else {
-                    cannot_cache!("unknown module output format", p.to_string_lossy().into_owned());
+                    cannot_cache!(
+                        "unknown module output format",
+                        p.to_string_lossy().into_owned()
+                    );
                 }
             }
             Some(NeedDepTarget) => {
