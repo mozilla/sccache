47d0d68f9f51d3dcbc38e361f4efd223c7b7a5e0
jullang <jullanggit@proton.me>
2026-01-28 14:38:48 +0100

    add sccache-dist to flake.nix

--- flake.nix
+++ flake.nix
@@ -6,74 +6,109 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-  }: let
-    cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
-    version = cargoToml.package.version;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    let
+      cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+      version = cargoToml.package.version;
 
-    mkSccache = final:
-      final.rustPlatform.buildRustPackage {
-        pname = "sccache";
-        inherit version;
+      mkSccache =
+        {
+          final,
+          pname,
+          features ? [ ],
+          description,
+        }:
+        final.rustPlatform.buildRustPackage {
+          pname = pname;
+          inherit version;
 
-        src = ./.;
+          src = ./.;
 
-        cargoLock = {
-          lockFile = ./Cargo.lock;
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+          nativeBuildInputs = [ final.pkg-config ];
+          buildInputs = [ final.openssl ];
+
+          cargoBuildFlags =
+            if features != [ ] then
+              [
+                "--features"
+                (builtins.concatStringsSep "," features)
+              ]
+            else
+              [ ];
+
+          doCheck = false;
+
+          meta = with final.lib; {
+            description = description;
+            homepage = "https://github.com/mozilla/sccache";
+            changelog = "https://github.com/mozilla/sccache/releases/tag/v${version}";
+            license = licenses.asl20;
+            mainProgram = "sccache";
+          };
         };
+    in
+    flake-utils.lib.eachSystem
+      [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+        "i686-linux"
+      ]
+      (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ self.overlays.default ];
+          };
+        in
+        {
+          packages = {
+            default = pkgs.sccache;
+            sccache = pkgs.sccache;
+            sccache-dist = pkgs.sccache-dist;
+          };
 
-        nativeBuildInputs = [final.pkg-config];
-        buildInputs = [final.openssl];
+          devShells.default = pkgs.mkShell {
+            name = "sccache-dev";
 
-        doCheck = false;
+            buildInputs = with pkgs; [
+              rustup
+              openssl
+              pkg-config
+              gcc
+            ];
+          };
 
-        meta = with final.lib; {
-          description = "Ccache with Cloud Storage";
-          homepage = "https://github.com/mozilla/sccache";
-          changelog = "https://github.com/mozilla/sccache/releases/tag/v${version}";
-          license = licenses.asl20;
-          mainProgram = "sccache";
-        };
-      };
-  in
-    flake-utils.lib.eachSystem [
-      "x86_64-linux"
-      "aarch64-linux"
-      "x86_64-darwin"
-      "aarch64-darwin"
-      "i686-linux"
-    ] (
-      system: let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [self.overlays.default];
-        };
-      in {
-        packages = {
-          default = pkgs.sccache;
-          sccache = pkgs.sccache;
-        };
-
-        devShells.default = pkgs.mkShell {
-          name = "sccache-dev";
-
-          buildInputs = with pkgs; [
-            rustup
-            openssl
-            pkg-config
-            gcc
-          ];
-        };
-
-        formatter = pkgs.alejandra;
-      }
-    )
+          formatter = pkgs.alejandra;
+        }
+      )
     // {
       overlays.default = final: prev: {
-        sccache = mkSccache final;
+        sccache = mkSccache {
+          inherit final;
+          pname = "sccache";
+          description = "Ccache with Cloud Storage";
+        };
+        sccache-dist = mkSccache {
+          inherit final;
+          pname = "sccache-dist";
+          features = [
+            "dist-client"
+            "dist-server"
+          ];
+          description = "Ccache with Cloud Storage and Distributed Compilation";
+        };
       };
     };
 }
