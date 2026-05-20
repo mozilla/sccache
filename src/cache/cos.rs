60612d83d58f767d82ad21b1d8a9a9800971659e
Philipp Krones <hello@philkrones.com>
2026-05-20 12:51:48 +0200

    Fix fallout in code
    - `HttpClientLayer` was removed and now has to be built from the
      `OperationContext`
    - `LoggingLayer` was moved to its own crate
    - `allow_anonymous` was deprecated in favor of `skip_signature`
    - Use `sha256` instead of `sha1`, as that is quasi-deprecated. I'm not
      sure if this is actually necessary. I think it fixed some issue in the
      tests? In any case, I think this is a good change
    - Check if `native_certs` are available when building a reqwest client,
      and if not set the TLS certs to an empty vector. With reqwest 0.13,
      the client would fail otherwise.

--- src/cache/cos.rs
+++ src/cache/cos.rs
@@ -11,8 +11,9 @@
 // limitations under the License.
 
 use opendal::Operator;
-use opendal::layers::{HttpClientLayer, LoggingLayer};
+use opendal::OperationContext;
 use opendal::services::Cos;
+use opendal_layer_logging::LoggingLayer;
 
 use crate::errors::*;
 
@@ -30,9 +31,8 @@ impl COSCache {
         }
 
         let op = Operator::new(builder)?
-            .layer(HttpClientLayer::new(set_user_agent()))
-            .layer(LoggingLayer::default())
-            .finish();
+            .with_context(OperationContext::new().with_http_transport(set_user_agent()))
+            .layer(LoggingLayer::default());
         Ok(op)
     }
 }
