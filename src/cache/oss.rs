897624547e029c0416c0aebde345720a9d2af15d
Philipp Krones <hello@philkrones.com>
2026-08-03 13:20:48 +0200

    Formatting (toml/rust)

--- src/cache/oss.rs
+++ src/cache/oss.rs
@@ -10,8 +10,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use opendal::Operator;
 use opendal::OperationContext;
+use opendal::Operator;
 use opendal::services::Oss;
 use opendal_layer_logging::LoggingLayer;
 
